7471539326d660745295649ec06b4403bcb26abf
Lemr <lemrovo@lemrovo.cz>
2012-05-07 10:34:32 +0200

    cleanup

--- installvenv.sh
+++ installvenv.sh
@@ -9,7 +9,5 @@ venv/bin/easy_install -U South
 venv/bin/easy_install -U django_extensions
 
 # OPTIONAL
-venv/bin/easy_install -U mechanize
-venv/bin/easy_install -U xmpppy
 venv/bin/easy_install -U python-gettext
 venv/bin/easy_install -U firepy
