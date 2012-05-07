12bb25aa3243a31cdce8cc36aca2fbdfa8c19167
Martin Samsula <msamsula@qdqmedia.com>
2012-05-07 11:11:29 +0200

    project bootstrap

--- bootstrap_project.sh
+++ bootstrap_project.sh
@@ -5,11 +5,20 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-HERE=`pwd`
+TEMPLATE_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 PROJECT_NAME=$1
+NEW_PROJECT_DIR=$TEMPLATE_DIR/../$PROJECT_NAME
+
+mkdir $NEW_PROJECT_DIR
+cd $NEW_PROJECT_DIR
+
+# template init
+cp -R $TEMPLATE_DIR/* ./
+git init
+git add .
+git ci -m 'template files'
 
 # project init
-git remote rm origin
 git mv web/duvenx web/$PROJECT_NAME
 git mv web/debian/duvenx.logrotate web/debian/$PROJECT_NAME.logrotate
 find . -name "*" -exec sed -i "s/duvenx/$PROJECT_NAME/g" {} \;
@@ -18,8 +27,8 @@ git ci -m "project $PROJECT_NAME init"
 
 # local settings
 cp web/$PROJECT_NAME/settings_local.tpl.py web/$PROJECT_NAME/settings_local.py
-find . -name "web/$PROJECT_NAME/settings_local.py" -exec sed -i "s/local_project_root/$HERE/g" {} \;
-mkdir -p $HERE/devices
+find . -name "web/$PROJECT_NAME/settings_local.py" -exec sed -i "s/local_project_root/$NEW_PROJECT_DIR/g" {} \;
+mkdir -p $NEW_PROJECT_DIR/devices
 
 # venv
 ./installvenv.sh
@@ -29,4 +38,3 @@ mkdir -p $HERE/devices
 
 # cleanup
 rm bootstrap_project.sh
-
